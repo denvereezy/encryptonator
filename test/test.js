e757412d73cb38eda869015190ff480739441de2
denver <denver@projectcodex.co>
2016-04-03 13:36:30 +0200

    const names updated

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
 const assert = require('assert');
-const encrypt = require('../encryptonator');
+const encryptonator = require('../encryptonator');
 
 describe("Testing my hash module", function() {
   it('should return a hash for any password inserted', function(done) {
     const password = 123;
-    encrypt.encryptPassword(password)
+    encryptonator.encryptPassword(password)
       .then(function(result) {
         console.log(result);
         assert(result);
@@ -14,9 +14,9 @@ describe("Testing my hash module", function() {
 
   it('should compare hash to inserted password and return true', function(done) {
     const password = 123;
-    encrypt.encryptPassword(password)
+    encryptonator.encryptPassword(password)
       .then(function(hash) {
-        return encrypt.comparePassword(password, hash);
+        return encryptonator.comparePassword(password, hash);
       })
       .then(function(match){
         console.log(match);
