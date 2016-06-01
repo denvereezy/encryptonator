6d6043755d7f9941716757e1b863ec4946111ee9
denver daniels <denver@projectcodex.co>
2016-06-01 10:17:07 +0200

    support added for using callbacks

--- index.js
+++ index.js
@@ -1,28 +1,57 @@
 const Promise = require('bluebird');
 const bcrypt = require('bcrypt');
 
-exports.encryptPassword = function(password) {
-  const passwordToEncrypt = password.toString();
-  return new Promise(function(resolve, reject) {
-    bcrypt.genSalt(10, function(err, salt) {
-      bcrypt.hash(passwordToEncrypt, salt, function(err, hash) {
-        if (err) {
-          return reject(err);
-        }
-        resolve(hash)
-      });
-    });
-  });
+exports.encryptPassword = function() {
+    const callback = arguments[1];
+    const passwordToEncrypt = arguments[0].toString();
+    switch (arguments.length) {
+        case 1:
+            return new Promise(function(resolve, reject) {
+                bcrypt.genSalt(10, function(err, salt) {
+                    bcrypt.hash(passwordToEncrypt, salt, function(err, hash) {
+                        if (err) {
+                            return reject(err);
+                        }
+                        resolve(hash)
+                    });
+                });
+            });
+            break;
+        case 2:
+            bcrypt.genSalt(10, function(err, salt) {
+                bcrypt.hash(passwordToEncrypt, salt, function(err, hash) {
+                    if (err) {
+                        return err;
+                    };
+                    callback(hash);
+                });
+            });
+            break;
+    };
 };
 
-exports.comparePassword = function(password, hash) {
-  const passwordToCompare = password.toString();
-  return new Promise(function(resolve, reject) {
-    bcrypt.compare(passwordToCompare, hash, function(err, pass) {
-      if (err) {
-        return reject(err);
-      }
-      resolve(pass)
-    });
-  });
+exports.comparePassword = function() {
+    const callback = arguments[2];
+    const passwordToCompare = arguments[0].toString();
+    const hash = arguments[1];
+    switch (arguments.length) {
+        case 2:
+            return new Promise(function(resolve, reject) {
+                bcrypt.compare(passwordToCompare, hash, function(err, pass) {
+                    if (err) {
+                        return reject(err);
+                    }
+                    resolve(pass)
+                });
+            });
+            break;
+        case 3:
+            bcrypt.compare(passwordToCompare, hash, function(err, pass) {
+                if (err) {
+                    return reject(err);
+                }
+                callback(pass);
+            });
+            break;
+    };
 };
